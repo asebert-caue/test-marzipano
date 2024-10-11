482175123a661f7dc27cb645e7abc9feebccdd28
aurelien sebert <asebert@caue-nord.com>
2024-10-11 10:04:28 +0200

    add marzipano export

---
+++ data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pinsard_-ars_vue_int_360",
+      "name": "PINSARD_ Ars_Vue_int_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8826346566067738,
+          "pitch": -0.0669234506089662,
+          "rotation": 0,
+          "target": "1-pinsard_-ars_vue_ext_360"
+        },
+        {
+          "yaw": 1.4326719890389743,
+          "pitch": 0.04839018383395377,
+          "rotation": 0,
+          "target": "2-pinsard_-bourg_vue_int_360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pinsard_-ars_vue_ext_360",
+      "name": "PINSARD_ Ars_Vue_ext_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.6421682756027405,
+        "pitch": -0.08344730745233875,
+        "fov": 1.6459122195236189
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.949835035076685,
+          "pitch": 0.0667826778740821,
+          "rotation": 0,
+          "target": "0-pinsard_-ars_vue_int_360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pinsard_-bourg_vue_int_360",
+      "name": "PINSARD_ Bourg_Vue_int_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6100710969419438,
+          "pitch": 0.0009812853413020406,
+          "rotation": 0,
+          "target": "0-pinsard_-ars_vue_int_360"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
